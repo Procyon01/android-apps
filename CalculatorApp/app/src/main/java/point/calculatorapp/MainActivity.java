089cf9f9a2161ad0dd2b1f26810551edce8f711c
Tyler McCarthy <mccartty@mail.gvsu.edu>
2016-01-28 22:37:17 -0500

    Calculator app initial layout finished

--- CalculatorApp/app/src/main/java/point/calculatorapp/MainActivity.java
+++ CalculatorApp/app/src/main/java/point/calculatorapp/MainActivity.java
@@ -1,7 +1,7 @@
 package point.calculatorapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
