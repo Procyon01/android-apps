7e527b5d622e715d675e40e29c3f936588e0b4cf
Tyler McCarthy <mccartty@mail.gvsu.edu>
2016-01-31 14:02:48 -0500

    CalculatorApp finished basic functionality

--- CalculatorApp/app/src/main/java/point/calculatorapp/MainActivity.java
+++ CalculatorApp/app/src/main/java/point/calculatorapp/MainActivity.java
@@ -2,12 +2,108 @@ package point.calculatorapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText operand1;
+    private EditText operand2;
+
+    private Button btnClear;
+    private Button btnAddition;
+    private Button btnSubtraction;
+    private Button btnMultiplication;
+    private Button btnDivision;
+
+    private TextView txtResult;
+
+    boolean verifyDoubles(){
+        if ((operand1.length() > 0) && (operand2.length() > 0)){
+            return true;
+        } else {
+            String msg = "Need numbers in both fields";
+            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+            return false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtResult = (TextView) findViewById(R.id.txtResult);
+
+        operand1 = (EditText) findViewById(R.id.editOperand1);
+        operand2 = (EditText) findViewById(R.id.editOperand2);
+
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnAddition = (Button) findViewById(R.id.btnAddition);
+        btnSubtraction = (Button) findViewById(R.id.btnSubtraction);
+        btnMultiplication = (Button) findViewById(R.id.btnMultiplication);
+        btnDivision = (Button) findViewById(R.id.btnDivision);
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operand1.setText("");
+                operand2.setText("");
+                txtResult.setText("0.0");323
+                operand1.requestFocus();
+            }
+        });
+
+        // Button listeners
+        btnAddition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (verifyDoubles()) {
+                    double oper1 = Double.parseDouble(operand1.getText().toString());
+                    double oper2 = Double.parseDouble(operand2.getText().toString());
+                    txtResult.setText(Double.toString(oper1 + oper2));
+               }
+            }
+        });
+
+        btnSubtraction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if (verifyDoubles()) {
+                    double oper1 = Double.parseDouble(operand1.getText().toString());
+                    double oper2 = Double.parseDouble(operand2.getText().toString());
+                    txtResult.setText(Double.toString(oper1 - oper2));
+                }
+            }
+        });
+
+        btnMultiplication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if (verifyDoubles()) {
+                    double oper1 = Double.parseDouble(operand1.getText().toString());
+                    double oper2 = Double.parseDouble(operand2.getText().toString());
+                    txtResult.setText(Double.toString(oper1 * oper2));
+                }
+            }
+        });
+
+        btnDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+                public void onClick(View v){
+                if (verifyDoubles()) {
+                    double oper1 = Double.parseDouble(operand1.getText().toString());
+                    double oper2 = Double.parseDouble(operand2.getText().toString());
+                    txtResult.setText(Double.toString(oper1 / oper2));
+                }
+            }
+        });
+
     }
+
+
+
 }
