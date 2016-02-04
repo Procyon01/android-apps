e5efd31028f5d59f4fdb6bac42e38670a46e4168
Tyler McCarthy <mccartty@mail.gvsu.edu>
2016-02-04 15:01:28 -0500

    Fix typo

--- CalculatorApp/app/src/main/java/point/calculatorapp/MainActivity.java
+++ CalculatorApp/app/src/main/java/point/calculatorapp/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 operand1.setText("");
                 operand2.setText("");
-                txtResult.setText("0.0");323
+                txtResult.setText("0.0");
                 operand1.requestFocus();
             }
         });
