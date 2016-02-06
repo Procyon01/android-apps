27cd61d9e8769663c482541907fd587ecd2f16ba
Tyler McCarthy <mccartty@mail.gvsu.edu>
2016-02-05 23:31:04 -0500

    Skeleton of GetRawData

--- FlickrBrowser/app/src/main/java/point/flickrbrowser/GetRawData.java
+++ FlickrBrowser/app/src/main/java/point/flickrbrowser/GetRawData.java
@@ -1,5 +1,10 @@
 package point.flickrbrowser;
 
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.net.HttpURLConnection;
+
 /**
  * Created by point on 2/4/2016.
  */
@@ -10,8 +15,43 @@ public class GetRawData {
     private String LOG_TAG = GetRawData.class.getSimpleName();
     private String mRawUrl;
     private String mData;
+    private DownloadStatus mDownloadStatus;
 
     public GetRawData(String mRawData) {
         this.mRawUrl = mRawData;
     }
+    
+    public void reset() {
+        this.mDownloadStatus = DownloadStatus.IDLE;
+        this.mRawUrl = null;
+        this.mData = null;
+    }
+
+    public DownloadStatus getDownloadStatus() {
+        return mDownloadStatus;
+    }
+
+    public String getmData() {
+        return mData;
+    }
+    
+    public class DownloadRawData extends AsyncTask<String, Void, String> {
+        
+        protected void onPostExecute(String webData){
+            
+        }
+        
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            
+            if (params == null){
+                return null;
+            }
+            
+            
+        }
+    }
+    
 }
